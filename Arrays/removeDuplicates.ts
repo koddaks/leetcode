ef19bc6e13c2caf21944d88f0070576ae7eae48e
koddaks <koss199710@gmail.com>
2024-08-16 15:08:31 +0300

    make array task  removeDuplicates

---
+++ Arrays/removeDuplicates.ts
@@ -0,0 +1,18 @@
+let nums = [0,0,1,1,1,2,2,3,3,4];
+           // [0,1,2,3,4,2,2,3,3,4]
+let removeDuplicates = function(nums: (number | string)[]): number {
+     let insertIndex = 1;
+        for(let i = 1; i < nums.length; i++){
+            
+            if(nums[i - 1] != nums[i]) {                
+                nums[insertIndex] = nums[i];     
+                insertIndex++;
+            }
+        }
+        for(let i = insertIndex; i < nums.length; i++) {
+            nums[i] = '_'
+        }
+    return insertIndex;
+};
+console.log(removeDuplicates(nums))
+console.log(nums)
